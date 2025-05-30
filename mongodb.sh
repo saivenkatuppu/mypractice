268fc3b4746f194deaab33f2f73792e454a31c25
sai <saivenkat67687@gmail.com>
2025-05-31 00:01:33 +0530

    mypractice

--- mongodb.sh
+++ mongodb.sh
@@ -31,7 +31,7 @@ VALIDATE(){
         exit 1
     fi
 }
-cp mongodb.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "copying mongodb"
 
 dnf install mongodb-org -y &>>$LOG_FILE
